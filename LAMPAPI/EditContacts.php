dd1dcf25e16e88247478a77059fe87c9e34888ee
TimKova <72708781+TimKova@users.noreply.github.com>
2023-09-03 16:13:50 -0400

    Update EditContacts.php
    Edited AddContact to make it update a contact instead.

--- LAMPAPI/EditContacts.php
+++ LAMPAPI/EditContacts.php
@@ -1,1 +1,55 @@
+<?php
+	$inData = getRequestInfo();
 
+	// Extract variables from the request payload
+	$phone = $inData["Phone"];
+	$email = $inData["Email"];
+	$name = $inData["Name"];
+	$userId = $inData["UserID"];
+  $id = $inData["ID"];
+
+	// Initialize database connection
+	$conn = new mysqli("localhost", "APIMan", "WeLoveAPIMan", "COP4331"); 	
+
+	// Check for connection error
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		// Prepare SQL statement for inserting data
+		$stmt = $conn->prepare("UPDATE Contacts SET Phone = ?, Email = ?, Name = ?, WHERE ID = ?");
+
+		// Bind the variables to the SQL statement
+		$stmt->bind_param("sss", $name, $email, $phone);
+
+		// Execute the SQL statement
+		$stmt->execute();
+
+		// Close the statement and connection
+		$stmt->close();
+		$conn->close();
+
+		// Return without any error
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
