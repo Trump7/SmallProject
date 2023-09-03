614a59a66fa173399ec43c2cbe0d8bc1e122e4a9
TimKova <72708781+TimKova@users.noreply.github.com>
2023-09-03 15:44:10 -0400

    Update DeleteContacts.php
    Modified AddContact.php for deletion purposes.

--- LAMPAPI/DeleteContacts.php
+++ LAMPAPI/DeleteContacts.php
@@ -1,1 +1,51 @@
+<?php
+	$inData = getRequestInfo();
 
+	// Extract variables from the request payload
+	$id = $inData["ID"];
+
+	// Initialize database connection
+	$conn = new mysqli("localhost", "APIMan", "WeLoveAPIMan", "COP4331"); 	
+
+	// Check for connection error
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		// Prepare SQL statement for inserting data
+		$stmt = $conn->prepare("DELETE from Contacts (ID) VALUES (?)");
+
+		// Bind the variables to the SQL statement
+		$stmt->bind_param("s", $id);
+
+		// Execute the SQL statement
+		$stmt->execute();
+
+		// Close the statement and connection
+		$stmt->close();
+		$conn->close();
+
+		// Return without any error
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
