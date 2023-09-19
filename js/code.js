842ebaabb25f278e9273c2131e95f9f1d17a0c58
Trump7 <ma332651@ucf.edu>
2023-09-19 14:38:37 -0400

    moved manager.js data to code.js.  Cleaned up code.js.  Modified scripts for color.html

--- js/code.js
+++ js/code.js
@@ -5,7 +5,7 @@ let userId = 0;
 let firstName = "";
 let lastName = "";
 
-
+//for home, about, and manager page
 function checkAuth() {
 	const {firstName, lastName, userId} = getUserData();
 	
@@ -33,9 +33,13 @@ function checkAuth() {
 		logoutButt.style.display = "none";
 		userNamePrompt.style.display = "none";
 		manButt.style.display = "none";
+		if(window.location.href = "color.html"){
+			window.location.href = "login.html";
+		}
 	}
 }
 
+//for login page
 function doLogin()
 {
 	userId = 0;
@@ -89,6 +93,7 @@ function doLogin()
 
 }
 
+//part of log in script
 function saveCookie()
 {
 	let minutes = 20;
@@ -97,39 +102,7 @@ function saveCookie()
 	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
 }
 
-function readCookie()
-{
-	userId = -1;
-	let data = document.cookie;
-	let splits = data.split(",");
-	for(var i = 0; i < splits.length; i++) 
-	{
-		let thisOne = splits[i].trim();
-		let tokens = thisOne.split("=");
-		if( tokens[0] == "firstName" )
-		{
-			firstName = tokens[1];
-		}
-		else if( tokens[0] == "lastName" )
-		{
-			lastName = tokens[1];
-		}
-		else if( tokens[0] == "userId" )
-		{
-			userId = parseInt( tokens[1].trim() );
-		}
-	}
-	
-	if( userId < 0 )
-	{
-		window.location.href = "index.html";
-	}
-	else
-	{
-		document.getElementById("userName").innerHTML = "Logged in as<br>" + firstName + " " + lastName;
-	}
-}
-
+//get user data is read cookie with returning the data
 function getUserData()
 {
 	userId = -1;
@@ -160,6 +133,7 @@ function getUserData()
 	return {firstName, lastName, userId};
 }
 
+//logout will only be available if logged in.  Auto expires cookie.
 function doLogout()
 {
 	userId = 0;
@@ -169,6 +143,115 @@ function doLogout()
 	window.location.href = "index.html";
 }
 
+//used to load contacts on manager page
+function loadContacts(){	
+	//just using this to get the userId
+	const {firstName, lastName, userId} = getUserData();
+	
+	//add contact
+	let data = {UserID: userId};
+	let jsonPayload = JSON.stringify(data);
+	
+	let url = urlBase + '/LoadContacts.' + extension;
+	let xhr = new XMLHttpRequest();
+	
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	
+	try{
+		xhr.onreadystatechange = function(){
+			if(this.readyState == 4 && this.status == 200){
+				let response = JSON.parse(xhr.responseText);
+				
+				if("results" in response){
+					let tableVals = '';
+					
+					for(let i = 0; i < response.results.length; i++){
+						tableVals += '<tr>';
+						tableVals += '<td>' + response.results[i].Name + '</td>';
+						tableVals += '<td>' + response.results[i].Phone + '</td>';
+						tableVals += '<td>' + response.results[i].Email + '</td>';
+						tableVals += '</tr>';
+					}
+					
+					document.getElementById("contacts-contents").innerHTML = tableVals;
+				}
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err){}
+}
+
+//used to open the add contact window
+function openAddContact(){
+	//show the add window
+	const window = document.getElementById('add-window');
+	window.style.display = 'block';
+}
+
+//used to clear and close the add contact window
+function closeAddContact(){
+	//hide the add window
+	const window = document.getElementById('add-window');
+	window.style.display = 'none';
+	//clear contents of add window
+	document.getElementById('name').value = '';
+	document.getElementById('phone').value = '';
+	document.getElementById('email').value = '';
+}
+
+//used to add a contact to that users' list
+function addContact(){
+	const name = document.getElementById('name').value;
+	const phone = document.getElementById('phone').value;
+	const mail = document.getElementById('email').value;
+	
+	//verification that name is present
+	if(name == ''){
+		document.getElementById('add-warning').innerHTML = "Name must be present";
+		return;
+	}
+	
+	//just using this to get the userId
+	const {firstName, lastName, userId} = getUserData();
+	
+	//add contact
+	let data = {Phone: phone, Email: mail, Name: name, UserID: userId};
+	let jsonPayload = JSON.stringify(data);
+	
+	let url = urlBase + '/AddContact.' + extension;
+	let xhr = new XMLHttpRequest();
+	
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	
+	try{
+		xhr.onreadystatechange = function(){
+			if(this.readyState == 4 && this.status == 200){
+				let response = JSON.parse(xhr.responseText);
+				
+				if("success" in response){
+					setTimeout(function(){document.getElementById('add-warning').innerHTML = "Account Added";}, 2000);
+					loadContacts()
+					closeAddContact();
+				}
+				else{
+					document.getElementById("add-warning").innerHTML = err.message;
+				}
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err){
+		document.getElementById("add-warning").innerHTML = err.message;
+	}
+}
+
+
+
+
+//not used
 function addColor()
 {
 	let newColor = document.getElementById("colorText").value;
@@ -199,7 +282,7 @@ function addColor()
 	}
 	
 }
-
+//not used
 function searchColor()
 {
 	let srch = document.getElementById("searchText").value;
