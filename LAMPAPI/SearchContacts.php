394bfe4041e59e3b668887ebc72d1c8bf0f63954
TimKova <72708781+TimKova@users.noreply.github.com>
2023-09-20 14:05:18 -0400

    Update SearchContacts.php
    Attempted to fix search for multiple variables.

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -12,10 +12,10 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("select * from Contacts where (Name like ?) and UserID=?");
+		$stmt = $conn->prepare("select * from Contacts where (Name like ?) and (Phone like ?) and (Email like ?) and (UserID=?)");
 		$colorName = "%" . $inData["search"] . "%";
-		//$stmt->bind_param("ss", $colorName, $inData["UserID"]);
-		$stmt->bind_param("ssss", $colorName, $colorName, $colorName, $inData["UserID"]);
+		$stmt->bind_param("ss", $colorName, $inData["UserID"]);
+		//$stmt->bind_param("ssss", $colorName, $colorName, $colorName, $inData["UserID"]);
 		$stmt->execute();
 		
 		$result = $stmt->get_result();
