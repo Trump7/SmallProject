78b51ede833d1422bce994413d537d913cf3c389
TimKova <72708781+TimKova@users.noreply.github.com>
2023-09-20 13:56:15 -0400

    Update SearchContacts.php
    Changed bind parameter statement to enable searching of email and phone.

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -14,7 +14,8 @@
 	{
 		$stmt = $conn->prepare("select * from Contacts where (Name like ?) and UserID=?");
 		$colorName = "%" . $inData["search"] . "%";
-		$stmt->bind_param("ss", $colorName, $inData["UserID"]);
+		//$stmt->bind_param("ss", $colorName, $inData["UserID"]);
+		$stmt->bind_param("ssss", $colorName, $colorName, $colorName, $inData["UserID"]);
 		$stmt->execute();
 		
 		$result = $stmt->get_result();
