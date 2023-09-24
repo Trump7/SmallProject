1307643de58af5ca359474167ab69e86b6600011
TimKova <72708781+TimKova@users.noreply.github.com>
2023-09-24 12:40:46 -0400

    Create searchJoke.php

---
+++ LAMPAPI/searchJoke.php
@@ -0,0 +1,65 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+	$id = $inData["ID"];
+
+	$conn = new mysqli("localhost", "APIMan", "WeLoveAPIMan", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("SELECT * FROM Jokes WHERE ID = ?");
+		
+		$stmt->bind_param("s", $id);
+		$stmt->execute();
+		
+		$result = $stmt->get_result();
+		
+		// checks for username
+		while($row = $result->fetch_assoc())
+		{
+			$searchCount++;
+      $searchResults = $row["Text"];
+		}
+		
+		if($searchCount == 0)
+		{
+			returnWithError('{"Error" : "No Matching Login"}');
+		}
+		else
+		{
+			returnWithInfo($login);
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError($err)
+	{
+		sendResultInfoAsJson($err);
+	}
+
+	function returnWithInfo($info)
+	{
+		$retValue = '{"Success": "' . $info . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+	
+?>
